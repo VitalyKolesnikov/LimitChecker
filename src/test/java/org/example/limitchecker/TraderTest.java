85f3022b7f6d97093e801ef898a474f3af688f70
Vitaly Kolesnikov <vitaly.kolesnikov88@gmail.com>
2020-11-12 12:42:04 +0300

    Add Trader tests

--- src/test/java/org/example/limitchecker/TraderTest.java
+++ src/test/java/org/example/limitchecker/TraderTest.java
@@ -1,28 +1,66 @@
 package org.example.limitchecker;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.example.limitchecker.model.CheckResult;
+import org.example.limitchecker.model.Order;
+import org.example.limitchecker.model.OrderTask;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.example.limitchecker.TestData.ORDER_LIST;
+import java.lang.reflect.Field;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class TraderTest {
 
     @Mock
     LimitChecker checker;
 
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-    }
+    @Mock
+    List<Order> orderList;
+
+    @Mock
+    Iterator<Order> orderIterator;
+
+    @InjectMocks
+    Trader trader;
 
     @Test
     void newTrader_ShouldBeRegistered_InChecker() {
-        new Trader(checker, ORDER_LIST);
-        verify(checker, times(1)).registerTrader();
+        verify(checker).registerTrader();
         verify(checker, never()).deregisterTrader();
     }
 
+    @Test
+    void submitOrderTask() throws InterruptedException {
+        trader.submitOrderTask(any(Order.class));
+        verify(checker).submitOrderTask(any(OrderTask.class));
+    }
+
+    @Test
+    void submitResult_ShouldAddResult_ToResultQueue() throws InterruptedException, NoSuchFieldException, IllegalAccessException {
+        Field field = trader.getClass().getDeclaredField("resultQueue");
+        field.setAccessible(true);
+        BlockingQueue<CheckResult> resultQueue = (BlockingQueue<CheckResult>) field.get(trader);
+        assertEquals(0, resultQueue.size());
+        trader.submitResult(mock(CheckResult.class));
+        assertEquals(1, resultQueue.size());
+    }
+
+    @Test
+    void trader_ShouldDeregister_WhenFinished() throws InterruptedException {
+        when(orderList.iterator()).thenReturn(orderIterator);
+        new Thread(trader).start();
+        new Thread(checker).start();
+        Thread.sleep(10);
+        verify(checker).deregisterTrader();
+    }
 }
